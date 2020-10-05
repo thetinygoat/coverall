9a4b6cc31b9a186ca7590ec621f1c536a33b6848
Sachin Saini <iamsainisachin@gmail.com>
2020-10-05 23:09:03 +0530

    add authenticity on story page

--- pages/search.js
+++ pages/search.js
@@ -6,13 +6,17 @@ import SearchCard from '../components/SearchCard';
 
 const search = () => {
   const [results, setResults] = useState([]);
+  const [loading, setloading] = useState(false);
   const handleSearch = (e) => {
+    setloading(true);
     if (e.target.value.length < 3) {
       setResults([]);
+      setloading(false);
       return;
     }
     searchUtil(e.target.value).then((res) => {
       setResults(res.articles);
+      setloading(false);
     });
   };
   const debounceSearch = (...args) => {
@@ -31,7 +35,7 @@ const search = () => {
           placeholder="Search for topics"
           onChange={debounceSearch(handleSearch, 500)}
         />
-        {results.length > 0 && <h1 className="text-2xl font-black pt-2 pb-4">Search Results</h1>}
+        {loading ? <div className="text-2xl font-black pt-2 pb-4">loading....</div> : results.length > 0 && <h1 className="text-2xl font-black pt-2 pb-4">Search Results</h1>}
         {results && results.map((article) => (
           <SearchCard
             author={article.author}
--- pages/story/[title].js
+++ pages/story/[title].js
@@ -1,19 +1,43 @@
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Head from 'next/head';
+import { ceil } from 'lodash';
 import Layout from '../../components/Layout';
+import predict from '../../api/predict';
 
-const Story = ({ url, content, title }) => (
-  <Layout>
-    <Head>
-      <title>{title}</title>
-    </Head>
-    <iframe title={url} srcDoc={content} className="h-screen mx-auto w-screen py-4" />
-  </Layout>
-);
+const Story = ({
+  url, content, title, score,
+}) => {
+  function calcClassName() {
+    if (1 - score < 0.3) {
+      return 'bg-red-400 mt-2 p-2 mb-2 rounded-sm text-2xl font-bold col-start-2 col-span-10';
+    } if (1 - score >= 0.3 && 1 - score <= 0.7) {
+      return 'bg-yellow-400 mt-2 p-2 mb-2 rounded-sm text-2xl font-bold col-start-2 col-span-10';
+    }
+    return 'bg-green-400 mt-2 p-2 mb-2rounded-sm text-2xl font-bold col-start-2 col-span-10';
+  }
+  return (
+    <Layout>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <div className={calcClassName(score)}>
+        {100 - ceil(parseFloat(score) * 100)}
+        % trusted
+      </div>
+      <iframe title={url} srcDoc={content} className="h-screen mx-auto w-screen py-4 col-start-0 col-span-10" />
+    </Layout>
+  );
+};
 
 export async function getServerSideProps({ query }) {
   const res = await axios.get(query.url);
-  return { props: { url: query.url, content: res.data, title: query.title } };
+  const data = await predict(query.url);
+  return {
+    props: {
+      url: query.url, content: res.data, title: query.title, score: data.score,
+    },
+  };
 }
 
 export default Story;
