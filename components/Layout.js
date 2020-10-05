8f38cd88e5ff61cd608c2f09964e4db72bfc14bf
Sachin Saini <iamsainisachin@gmail.com>
2020-10-05 23:49:36 +0530

    fix title

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import Navbar from './Navbar';
 const Layout = ({ children }) => (
   <div className="grid grid-cols-12">
     <Head>
-      <title>News Zen | Get latest and trusted news stories</title>
+      <title>Coverall | Get latest and trusted news stories</title>
       <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js" />
 
     </Head>
